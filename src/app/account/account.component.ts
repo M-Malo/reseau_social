ad97f6dd2923c1b951860daf33dbd6f3e0f171b5
melina <melinaloyant@gmail.com>
2024-03-21 10:00:34 +0100

    routage vers back

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { User } from '../model/user';
+import { Event } from '../model/event';
+import { UsersBackService } from '../users-back.service';
+import { EventsBackService } from '../events-back.service';
 
 @Component({
   selector: 'app-account',
@@ -7,16 +10,47 @@ import { User } from '../model/user';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
-  nbEventFavoris:number = 5
-  utilisateur:User = new User("0","Username","mail","password","nom","prenom","2000-01-01","",false)
-  eventList = ["Bowling entre copaing","Laser Game","Anniversaire a la casa avec tous les copains du monde entier !!"]
+  nbEventFavoris: number = 5
+  utilisateur: User = new User("0","Username","mail","password","nom","prenom","2000-01-01",false, "image")
+  eventList : String[] = [];
+  //eventList = ["Bowling entre copaing","Laser Game","Anniversaire a la casa avec tous les copains du monde entier !!"]
 
 
-  constructor() {
-    this.utilisateur = new User("0","M-Malo","malo.guetto@isen-ouest.yncrea.fr","123poulet","Guetto","Malo","2001-02-28","",true)
+  constructor(private userBackservice: UsersBackService, private eventBackservice: EventsBackService) {
+    this.utilisateur = new User("0","M-Malo","malo.guetto@isen-ouest.yncrea.fr","123poulet","Guetto","Malo","2001-02-28",true, "image")
+    // this.getUser();
+    // this.getEvents();
   }
 
   logValueDate() {
-    console.log(this.utilisateur.dateDeNaissance)
+    console.log(this.utilisateur.date_naissance)
+  }
+
+  getUser() {
+
+    this.userBackservice.getUserById("1").subscribe( //TODO comment récupérer userId ??
+      (user: User) => {
+        this.utilisateur = user;
+        console.log(this.utilisateur);
+      },
+      (error) => {
+        console.error('Une erreur s\'est produite lors de la récupération des événements :', error);
+      }
+    );
+  }
+
+  getEvents() {
+
+    this.eventBackservice.getEventsByUser("1").subscribe( //TODO comment récupérer userId ??
+      (events: Event[]) => {
+        for (let event of events) {
+          this.eventList.push(event.nom);
+        }
+        console.log(this.eventList);
+      },
+      (error) => {
+        console.error('Une erreur s\'est produite lors de la récupération des événements :', error);
+      }
+    );
   }
 }
--- src/app/event/event-view/event-view.component.ts
+++ src/app/event/event-view/event-view.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { EventsBackService } from 'src/app/events-back.service';
 import { Event } from 'src/app/model/event';
 
 @Component({
@@ -8,20 +9,28 @@ import { Event } from 'src/app/model/event';
   styleUrls: ['./event-view.component.css']
 })
 export class EventViewComponent {
-  event = new Event(0,0,"Anniversaire",3,"../../../assets/images/poolparty.jpg",5,"2024-02-28","Salut Salut, on va feter mon anniv à la casa avec les copaings, Venez nombreux cela va etre sper sympas. N’oubliez pas de venir avec un SAM")
+  event = new Event("","","Anniversaire",3,"../../../assets/images/poolparty.jpg",5,"2024-02-28","Salut Salut, on va feter mon anniv à la casa avec les copaings, Venez nombreux cela va etre sper sympas. N’oubliez pas de venir avec un SAM")
   id: string = "0";
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private eventBackservice: EventsBackService, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = params['id']; // Récupère l'ID de l'URL
     });
+    this.getEvent();
   }
 
-  navigate() {
-    const url = "eventForm"
-    this.router.navigateByUrl(url);
-    this.router.navigate([url, this.id]);
+  getEvent() {
+
+    this.eventBackservice.getEventById(this.id).subscribe(
+      (event: Event) => {
+        this.event = event;
+        console.log(this.event);
+      },
+      (error) => {
+        console.error('Une erreur s\'est produite lors de la récupération de l\'événement :', error);
+      }
+    );
   }
 }
